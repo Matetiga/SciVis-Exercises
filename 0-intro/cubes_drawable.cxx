872071da6bc47d5219763044cf3caf4bf5084db0
Kittycat <sankhayan2002@gmail.com>
2026-04-22 20:02:17 +0200

    feat: assignment 0.3a done

--- 0-intro/cubes_drawable.cxx
+++ 0-intro/cubes_drawable.cxx
@@ -78,6 +78,10 @@ protected:
 	// for the fractal structure
 	cubes_fractal fractal;
 
+	std::vector<vertex> all_vertices;
+	cgv::render::vertex_buffer vb_all;
+	cgv::render::attribute_array_binding vao_all;
+
 public:
 	cubes_drawable():
 		fb_bgcolor_r(0.8f), fb_bgcolor_g(0.8f), fb_bgcolor_b(0.1f),
@@ -211,9 +215,93 @@ public:
 			fractal.use_vertex_array(&vertex_array, vertices.size(), GL_TRIANGLES);
 		else if (mode == NON_INTERLEAVED)
 			fractal.use_vertex_array(&vao_non_interleaved, vertices.size(), GL_TRIANGLES);
+		
 
 		if (this->is_visible())
 			post_redraw();
+	}
+
+	void build_fractal_geometry(
+		const cgv::math::fmat<double, 4, 4>& transform,
+		int max_depth,
+		int level
+	)
+	{
+		auto T = transform * cgv::math::scale4<double>(0.5, 0.5, 0.5);
+
+		for (auto& v : vertices) {
+			vertex out;
+
+			cgv::vec3 p = v.pos;
+			out.pos = cgv::vec3(
+				T(0, 0) * p.x() + T(0, 1) * p.y() + T(0, 2) * p.z() + T(0, 3),
+				T(1, 0) * p.x() + T(1, 1) * p.y() + T(1, 2) * p.z() + T(1, 3),
+				T(2, 0) * p.x() + T(2, 1) * p.y() + T(2, 2) * p.z() + T(2, 3)
+			);
+
+			out.normal = v.normal;
+			out.normal.normalize();
+
+			all_vertices.push_back(out);
+		}
+
+		if (level >= max_depth)
+			return;
+
+		int num_children = (level == 0) ? 4 : 3;
+
+		for (int i = 0; i < num_children; i++) {
+			auto childT =
+				T *
+				cgv::math::rotate4<double>(i * 90 - 90, 0, 0, 1) *
+				cgv::math::translate4<double>(2, 0, 0);
+
+			build_fractal_geometry(childT, max_depth, level + 1);
+		}
+	}
+
+	void regenerate_geometry(cgv::render::context& ctx)
+	{
+		all_vertices.clear();
+
+		cgv::math::fmat<double, 4, 4> I;
+		I.identity();
+
+		// TODO: Set the recursion depth via config file after implementing Task 0.2a and 0.2b
+		build_fractal_geometry(I, 4, 0);
+
+		if (all_vertices.empty())
+			return;
+
+		auto vec3type =
+			cgv::render::element_descriptor_traits<cgv::vec3>
+			::get_type_descriptor(all_vertices[0].pos);
+
+		vb_all.destruct(ctx);
+		vao_all.destruct(ctx);
+
+		vb_all.create(ctx, &all_vertices[0], all_vertices.size());
+		vao_all.create(ctx);
+
+		auto& shader = ctx.ref_surface_shader_program(true);
+
+		vao_all.set_attribute_array(
+			ctx, shader.get_position_index(),
+			vec3type, vb_all,
+			0,
+			all_vertices.size(),
+			sizeof(vertex)
+		);
+
+		vao_all.set_attribute_array(
+			ctx, shader.get_normal_index(),
+			vec3type, vb_all,
+			sizeof(cgv::vec3),
+			all_vertices.size(),
+			sizeof(vertex)
+		);
+
+
 	}
 
 	// this was only necessary to for the screen resolution values in demo
@@ -305,6 +393,17 @@ public:
 			fractal.use_vertex_array(&vertex_array, vertices.size(), GL_TRIANGLES);
 		else if (mode == NON_INTERLEAVED)
 			fractal.use_vertex_array(&vao_non_interleaved, vertices.size(), GL_TRIANGLES);
+		else if (mode == SINGLE_VERTEX_BUFFER) {
+			regenerate_geometry(ctx);
+
+			cgv::media::illum::surface_material mat;
+			mat.diffuse_reflectance = cgv::rgb(1.0f, 0.5f, 0.5f);
+			ctx.set_material(mat);
+
+			vao_all.enable(ctx);
+			glDrawArrays(GL_TRIANGLES, 0, (GLsizei)all_vertices.size());
+			vao_all.disable(ctx);
+		}
 
 
 		return success;
@@ -312,6 +411,8 @@ public:
 
 	void draw(cgv::render::context& ctx)
 	{
+
+		
 
 		glPushAttrib(GL_COLOR_BUFFER_BIT | GL_VIEWPORT_BIT | GL_POLYGON_BIT);
 		glClearColor(bgcolor.R(), bgcolor.G(), bgcolor.B(), bgcolor.alpha());
@@ -328,8 +429,21 @@ public:
 
 
 		//ctx.tesselate_unit_square();
-		draw_my_unit_cube(ctx);
-		fractal.draw_recursive(ctx, cgv::rgb(1.0f, 0.5f, 0.5f), 3, 0);
+		if (mode == SINGLE_VERTEX_BUFFER) {
+			regenerate_geometry(ctx);
+
+			cgv::media::illum::surface_material mat;
+			mat.diffuse_reflectance = cgv::rgb(1.0f, 0.5f, 0.5f);
+			ctx.set_material(mat);
+
+			vao_all.enable(ctx);
+			glDrawArrays(GL_TRIANGLES, 0, (GLsizei)all_vertices.size());
+			vao_all.disable(ctx);
+		}
+		else {
+			draw_my_unit_cube(ctx);
+			fractal.draw_recursive(ctx, cgv::rgb(1.0f, 0.5f, 0.5f), 3, 0);
+		}
 		
 		
 
